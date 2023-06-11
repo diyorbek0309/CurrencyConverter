547bd900ce60b29058c8ccd2a379ff4852120354
Diyorbek Olimov <60889787+diyorbek0309@users.noreply.github.com>
2023-06-11 22:09:12 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const URL =
+  "https://api.currencyapi.com/v3/latest?apikey=NdQ8IZN4TeRBql6Pr1jXhs67DMg3hP50VnCajqUF";
+
+// fetch(URL)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data.data.EUR);
+//   });

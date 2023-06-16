ffeb92a6ae1726ef5b703d1e3876e91242fa5d83
Diyorbek Olimov <60889787+diyorbek0309@users.noreply.github.com>
2023-06-16 22:26:31 +0500

    removed key

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const URL =
-  "https://api.currencyapi.com/v3/latest?apikey=NdQ8IZN4TeRBql6Pr1jXhs67DMg3hP50VnCajqUF";
+  "https://api.currencyapi.com/v3/latest?apikey=";
 
 // fetch(URL)
 //   .then((res) => res.json())
